54fd0bdacfda3cd5ba573266bf4e97a38f9ae0ad
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-15 22:58:31 +1200

    human-readable hours and minutes calculation

--- src/Components/FilmInfo.js
+++ src/Components/FilmInfo.js
@@ -1,17 +1,40 @@
+// Calculate runtime in human-understandable "hours and minutes" phrasing
+function toHoursAndMins(runtimeMins) {
+  // Calculate number of whole hours in runtime
+  const wholeHours = Math.floor(runtimeMins / 60);
+
+  // Calculate modulus of minutes (number of minutes left over after full hours calculated)
+  const remainderMins = runtimeMins % 60;
+
+  // Return whole hours, with "hour" singular if only one hour, "hours" plural if more than one, plus minutes
+  return (
+    wholeHours +
+    (wholeHours === 1 ? " hour " : " hours ") +
+    remainderMins +
+    " min."
+  );
+}
+
 function FilmInfo(props) {
   return (
     <article className="film-container">
-      <div className="film-data pb-5 md:pb-10">
-        <h3 className="film-title text-2xl font-bold italic text-center">
+      <div className="film-data xs:pb-5 md:pb-6">
+        <h3 className="film-title text-2xl font-bold italic text-center pb-2">
           {props.title}
         </h3>
         <div className="film-metadata text-sm text-center">
-          Directed by {props.director} &bull; {props.releaseYear} &bull;{" "}
-          {props.runtime}
-          &nbsp; mins.
+          Directed by {props.director}&nbsp;&bull;&nbsp;{props.releaseYear}
+          &nbsp;&bull;&nbsp;{props.runtimeMins} min.
+          {/* Conditionally render human-readable runtime if > 60 mins */}
+          {/* Hidden on small screens, visible on md and up */}
+          {props.runtimeMins > 60 && (
+            <span className="hidden md:inline">
+              &nbsp;({toHoursAndMins(props.runtimeMins)})
+            </span>
+          )}
         </div>
       </div>
-      <div className="synopsis-image pb-5 flex flex-col-reverse md:flex-row sm:space-y-6">
+      <div className="synopsis-image pb-5 flex flex-col-reverse md:flex-row pt-6">
         <p className="synopsis mx-8 sm:mx-24 md:mx-10 my-auto md:mr-10">
           {props.synopsis}
         </p>
@@ -23,7 +46,7 @@ function FilmInfo(props) {
           width={356}
         />
       </div>
-      <div className="trailer-imdb mx-10 space-x-5 space-y-5 md:space-y-0 flex flex-col items-center md:flex-row">
+      <div className="trailer-imdb mx-10 space-x-5 space-y-5 pt-6 md:space-y-0 md:pt-0 flex flex-col items-center md:flex-row">
         <span className="imdb">
           <a
             href={`https://www.imdb.com/title/tt${props.imdbID}`}
--- src/Components/NowPlaying.js
+++ src/Components/NowPlaying.js
@@ -17,7 +17,7 @@ const filmData = [
     title: "12 Angry Men",
     director: "Sidney Lumet",
     releaseYear: 1957,
-    runtime: 96,
+    runtimeMins: 96,
     imdbID: "0050083",
     synopsis:
       "Following the closing arguments in a murder trial, the 12 members of the jury must deliberate, with a guilty verdict meaning death for the accused, an inner-city teen. As the dozen men try to reach a unanimous decision while sequestered in a room, one juror (Henry Fonda) casts considerable doubt on elements of the case. Personal issues soon rise to the surface, and conflict threatens to derail the delicate process that will decide one boy's fate.",
@@ -28,7 +28,7 @@ const filmData = [
     title: "Psycho",
     director: "Alfred Hitchcock",
     releaseYear: 1960,
-    runtime: 109,
+    runtimeMins: 109,
     imdbID: "0054215",
     synopsis:
       "Phoenix secretary Marion Crane (Janet Leigh), on the lam after stealing $40,000 from her employer in order to run away with her boyfriend, Sam Loomis (John Gavin), is overcome by exhaustion during a heavy rainstorm. Traveling on the back roads to avoid the police, she stops for the night at the ramshackle Bates Motel and meets the polite but highly strung proprietor Norman Bates (Anthony Perkins), a young man with an interest in taxidermy and a difficult relationship with his mother. ",
@@ -39,7 +39,7 @@ const filmData = [
     title: "Rear Window",
     director: "Alfred Hitchcock",
     releaseYear: 1954,
-    runtime: 112,
+    runtimeMins: 112,
     imdbID: "0047396",
     synopsis:
       "The story of a recuperating news photographer who believes he has witnessed a murder. Confined to a wheelchair after an accident, he spends his time watching the occupants of neighbouring apartments through a telephoto lens and binoculars and becomes convinced that a murder has taken place.",
@@ -50,7 +50,7 @@ const filmData = [
     title: "Vertigo",
     director: "Alfred Hitchcock",
     releaseYear: 1958,
-    runtime: 128,
+    runtimeMins: 128,
     imdbID: "0052357",
     synopsis:
       "An ex-police officer who suffers from an intense fear of heights is hired to prevent an old friend's wife from committing suicide, but all is not as it seems. Hitchcock's haunting, compelling masterpiece is uniquely revelatory about the director's own predilections and hang-ups and is widely considered to be one of his masterworks.",
@@ -61,7 +61,7 @@ const filmData = [
     title: "Seven Samurai",
     director: "Akira Kurosawa",
     releaseYear: 1954,
-    runtime: 207,
+    runtimeMins: 207,
     imdbID: "0047478",
     synopsis:
       "A samurai answers a village's request for protection after he falls on hard times. The town needs protection from bandits, so the samurai gathers six others to help him teach the people how to defend themselves, and the villagers provide the soldiers with food. A giant battle occurs when 40 bandits attack the village.",
@@ -79,10 +79,10 @@ function NowPlaying() {
         className="h-[96px] w-[96px] m-auto"
       />
       {/* Map over the `filmData` array, creating a `FilmInfo` component for each object */}
-      {filmData.map((film) => {
-        // Set the key to "film0", etc., and use the spread operator to send all other key-value pairs as props
-        return <FilmInfo key={"film" + film.id} {...film} />;
-      })}
+      {filmData.map((film) => (
+        // Map over each item in the array, sending "film0" etc. as ID, and every other element using the spread operator (...)
+        <FilmInfo key={"film" + film.id} {...film} />
+      ))}
     </main>
   );
 }
