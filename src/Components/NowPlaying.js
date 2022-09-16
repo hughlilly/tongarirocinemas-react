6240a69147048aee4db9d154dd7b4e198c3bdd70
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-16 15:53:31 +1200

    add imdb IDs

--- src/Components/NowPlaying.js
+++ src/Components/NowPlaying.js
@@ -18,7 +18,8 @@ const filmData = [
     director: "Sidney Lumet",
     releaseYear: 1957,
     runtimeMins: 96,
-    imdbID: "0050083",
+    IMDbID: "0050083",
+    trailerID: "iWh0pONVnjc",
     synopsis:
       "Following the closing arguments in a murder trial, the 12 members of the jury must deliberate, with a guilty verdict meaning death for the accused, an inner-city teen. As the dozen men try to reach a unanimous decision while sequestered in a room, one juror (Henry Fonda) casts considerable doubt on elements of the case. Personal issues soon rise to the surface, and conflict threatens to derail the delicate process that will decide one boy's fate.",
     filmStill: TwelveAngryMenStill,
@@ -29,7 +30,8 @@ const filmData = [
     director: "Alfred Hitchcock",
     releaseYear: 1960,
     runtimeMins: 109,
-    imdbID: "0054215",
+    IMDbID: "0054215",
+    trailerID: "DTJQfFQ40lI",
     synopsis:
       "Phoenix secretary Marion Crane (Janet Leigh), on the lam after stealing $40,000 from her employer in order to run away with her boyfriend, Sam Loomis (John Gavin), is overcome by exhaustion during a heavy rainstorm. Traveling on the back roads to avoid the police, she stops for the night at the ramshackle Bates Motel and meets the polite but highly strung proprietor Norman Bates (Anthony Perkins), a young man with an interest in taxidermy and a difficult relationship with his mother. ",
     filmStill: PsychoStill,
@@ -40,7 +42,8 @@ const filmData = [
     director: "Alfred Hitchcock",
     releaseYear: 1954,
     runtimeMins: 112,
-    imdbID: "0047396",
+    IMDbID: "0047396",
+    trailerID: "Ob_Sq__g01E",
     synopsis:
       "The story of a recuperating news photographer who believes he has witnessed a murder. Confined to a wheelchair after an accident, he spends his time watching the occupants of neighbouring apartments through a telephoto lens and binoculars and becomes convinced that a murder has taken place.",
     filmStill: RearWindowStill,
@@ -51,7 +54,8 @@ const filmData = [
     director: "Alfred Hitchcock",
     releaseYear: 1958,
     runtimeMins: 128,
-    imdbID: "0052357",
+    IMDbID: "0052357",
+    trailerID: "95o-QM-lz8g",
     synopsis:
       "An ex-police officer who suffers from an intense fear of heights is hired to prevent an old friend's wife from committing suicide, but all is not as it seems. Hitchcock's haunting, compelling masterpiece is uniquely revelatory about the director's own predilections and hang-ups and is widely considered to be one of his masterworks.",
     filmStill: VertigoStill,
@@ -62,7 +66,8 @@ const filmData = [
     director: "Akira Kurosawa",
     releaseYear: 1954,
     runtimeMins: 207,
-    imdbID: "0047478",
+    IMDbID: "0047478",
+    trailerID: "7mw6LyyoeGE",
     synopsis:
       "A samurai answers a village's request for protection after he falls on hard times. The town needs protection from bandits, so the samurai gathers six others to help him teach the people how to defend themselves, and the villagers provide the soldiers with food. A giant battle occurs when 40 bandits attack the village.",
     filmStill: SevenSamuraiStill,
