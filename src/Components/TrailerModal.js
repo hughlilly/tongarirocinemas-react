6da378ec05f122253603d7f1c5c70295611c378a
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-16 16:15:47 +1200

    good enough

--- src/Components/TrailerModal.js
+++ src/Components/TrailerModal.js
@@ -17,34 +17,31 @@ function TrailerModal({ trailerID, modalOpen, closeModal }) {
   return (
     modalOpen &&
     createPortal(
-      <>
+      <dialog
+        id="modal"
+        className="modal border border-[#333] aspect-video w-[95vw] max-w-[768px] lg:max-h-[75vh] p-0 bg-transparent"
+        ref={dialogRef}
+      >
         <button
           id="close-btn"
-          className="flex justify-end rounded bg-black text-xl font-bold text-white cursor-pointer absolute"
+          className="text-white text-xl font-bold pb-2 pr-1 cursor-pointer bg-transparent absolute right-0"
           onClick={closeModal}
         >
-          &times;close
+          ×
         </button>
-        <dialog
-          id="modal"
-          className="modal border border-[#333] aspect-video w-[95vw] max-w-[768px] lg:max-h-[75vh] p-0"
-          ref={dialogRef}
-          onCancel={closeModal}
-        >
-          <iframe
-            id="embedded-trailer"
-            width="100%"
-            height="100%"
-            // Setting `enablejsapi` to 1 (true) allows us to pause the video on close; see
-            // https://developers.google.com/youtube/player_parameters
-            // src={`https://www.youtube.com/embed/${trailerID}?autoplay=1&enablejsapi=1`}
-            src={`https://www.youtube.com/embed/${trailerID}?&enablejsapi=1`}
-            title="YouTube video player"
-            frameBorder="0"
-            allowFullScreen
-          ></iframe>
-        </dialog>
-      </>,
+        <iframe
+          id="embedded-trailer"
+          width="100%"
+          height="100%"
+          // Setting `enablejsapi` to 1 (true) allows us to pause the video on close; see
+          // https://developers.google.com/youtube/player_parameters
+          // src={`https://www.youtube.com/embed/${trailerID}?autoplay=1&enablejsapi=1`}
+          src={`https://www.youtube.com/embed/${trailerID}?&enablejsapi=1`}
+          title="YouTube video player"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </dialog>,
       document.getElementById("modal-root")
     )
   );
