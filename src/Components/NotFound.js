bcdd352bc8969a5275dfd57e2a872991a43a8b04
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-18 18:06:59 +1200

    styling

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function NotFound() {
-  return (
-    <h1 className="flex flex-1 items-center justify-center h-full">
-      404 Not Found
-    </h1>
-  );
+  return <h1 className="text-center">404 Not Found</h1>;
 }
 
 export default NotFound;
