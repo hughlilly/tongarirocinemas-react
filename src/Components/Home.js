7976efef752779de27e6c5c10b6ff97e2b789aae
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-18 18:46:20 +1200

    consolidate comments; rm keys from `hr` elements

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,19 +11,16 @@ function Home() {
       {/* Carousel */}
       <Carousel />
       {/* Home page sections */}
+      {/* Map over the `sections` array, creating a `HomeSection` component for each object */}
       <main className="m-auto md:px-14 max-w-4xl text-center md:text-left flex flex-col space-y-10 md:space-y-20 pt-16 first-of-type:md:pt-24 pb-28 last-of-type:md:pb-36">
         {sections.map((section, index) => {
-          // If we are not yet at the last element, render a horizontal rule between HomeSection components
-          // Give every element a unique
+          // Render a horizontal rule between HomeSection components (but not after the last one)
+          // Give every major element, including invisible Fragments, a unique key
           if (index < sections.length - 1) {
             return (
               <Fragment key={"Fragment" + section.id}>
                 <HomeSection {...section} key={"HomeSection" + section.id} />
-                <hr
-                  width="65%"
-                  className="mx-auto my-10"
-                  key={"HorizontalRule" + section.id}
-                />
+                <hr width="65%" className="mx-auto my-10" />
               </Fragment>
             );
           } else {
--- src/Components/NowPlaying.js
+++ src/Components/NowPlaying.js
@@ -15,15 +15,12 @@ function NowPlaying() {
       />
       {/* Map over the `filmData` array, creating a `FilmInfo` component for each object */}
       {filmData.map((film, index) => {
-        // If we are not at the last object in the array, return a fragment with the FilmInfo component and an <hr> element
+        // Render a horizontal rule between FilmInfo components (but not after the last one)
+        // Give every major element, including invisible Fragments, a unique key
         return index != filmData.length - 1 ? (
           <Fragment key={"Fragment" + film.id}>
             <FilmInfo {...film} key={"FilmInfoSection" + film.id} />
-            <hr
-              width="65%"
-              className="mx-auto my-10"
-              key={"HorizontalRule" + film.id}
-            />
+            <hr width="65%" className="mx-auto my-10" />
           </Fragment>
         ) : (
           // Else, for the final object in the array, return just the FilmInfo component
